7c2539a92c7f9ed1b4f052871252f05f89133a06
anshuang <1205697836@qq.com>
2016-05-12 11:43:52 +0800

    1.5
    1.5

--- ViewController.swift
+++ ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var ydf: UILabel!
     var x:Int = 0
+    var l:Int = 0
     @IBAction func ydjf(sender: UIButton) {
         x++
         ydf.text="\(x)"
@@ -43,11 +44,18 @@ class ViewController: UIViewController {
 
     }
     
+    @IBAction func qingkong(sender: UIButton) {
+        
+        ydf.text="\(l)"
+        jdf.text="\(l)"
+        
+    }
     
     
     
-    //
-    //
+    //1.点击队伍按钮要能获得球队数据2如果点击加一分按钮就应该记录是谁犯规，并记录犯规次数，罚下时做提醒
+    //3.最后通过加分按钮的响应按下次数来统计本场得分的综合数据
+    //4.按下按钮值应该直接存入数据库，而显示的分的页面是查询数据库之后得到的得分
     var db:SQLiteDB!
     
     @IBOutlet var txtUname: UITextField!
@@ -59,13 +67,18 @@ class ViewController: UIViewController {
         //获取数据库实例
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        db.execute("create table if not exists t_user(uid integer primary key,jiadjy varchar(20)")
         //如果有数据则加载
         initUser()
     }
     
     //点击保存
-    @IBAction func saveClicked(sender: AnyObject) {
+    //保存1，第二次保存2  比分数据变成3  应该存入2   掉用数据库最后一条记录用当前比分减去最后一条记录在存入
+   // @IBAction func saveClicked(sender: AnyObject) {
+   //     saveUser()
+   // }
+    
+    @IBAction func jiajybc(sender: AnyObject) {
         saveUser()
     }
     
@@ -75,17 +88,19 @@ class ViewController: UIViewController {
         if data.count > 0 {
             //获取最后一行数据显示
             let user = data[data.count - 1]
-            txtUname.text = user["uname"] as? String
-            txtMobile.text = user["mobile"] as? String
+            jdf.text=user["jiadjy"] as? String
+            //txtUname.text = user["uname"] as? String
+            //txtMobile.text = user["mobile"] as? String
         }
     }
     
     //保存数据到SQLite
     func saveUser() {
-        let uname = self.txtUname.text!
-        let mobile = self.txtMobile.text!
+        //甲乙两队得分数据变量
+        var jiadjy = 0
+        jiadjy++
         //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
-        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        let sql = "insert into t_user(jiadjy) varchar('\(jiadjy)')"
         print("sql: \(sql)")
         //通过封装的方法执行sql
         let result = db.execute(sql)
